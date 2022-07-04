886d6182a7d74388003b36519f944bbbea93e1d8
Kysis321 <72172415+Kysis321@users.noreply.github.com>
2022-07-04 14:13:50 +1000

    TextUI

---
+++ Assets/TextFacesCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFacesCamera : MonoBehaviour
+{
+    public Camera mainCamera;
+
+
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+
+
+
+    }
+}
