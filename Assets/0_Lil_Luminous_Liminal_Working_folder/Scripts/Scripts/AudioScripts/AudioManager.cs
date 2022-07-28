39ea0e935b2c77f9588fc4b15c3a57b36783aab1
Lilya030 <broughton.rj@gmail.com>
2022-07-28 19:32:47 +1000

    Audio scripting

--- Assets/0_Lil_Luminous_Liminal_Working_folder/Scripts/Scripts/AudioScripts/AudioManager.cs
+++ Assets/0_Lil_Luminous_Liminal_Working_folder/Scripts/Scripts/AudioScripts/AudioManager.cs
@@ -1,15 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public SFXSound[] sfxSounds;
+    public AudioMixerGroup mixerGroup;
+
+
     [SerializeField] private AudioSource[] audioSources;
     [SerializeField] private AudioClip[] SfxClips;
     [SerializeField] private AudioClip[] voiceLines;
     [SerializeField] private AudioSource voSource;
 
     //TwentyThree,TwentyFour,TwentyFive,TwentySix,End
+
+    public void Awake()
+    {
+       foreach (SFXSound s in sfxSounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = mixerGroup;
+		}  
+    }
+
+
+    public void PlayEnter()
+    {
+        Play("ButtonEnter");
+    }
+
+
+
+
+
+    public void Play(string sound)
+    {
+        SFXSound s = Array.Find(sfxSounds, item => item.name == sound);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));  //I believe this is randomising the volume/pitch by allocating a random number from the volume variance number -/+ 2
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+        s.source.Play();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     private void OnEnable() //
     {
         EventManager.sfxEvent += playSfx;
